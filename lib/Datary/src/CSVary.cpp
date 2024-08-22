062a3ffd6afe61e7538fae5f0cf0b1f235e458bf
Alejandro Drausal <a.drausal@ufl.edu>
2024-08-22 13:04:09 -0400

    Moved CSVary out to a header and cpp

---
+++ lib/Datary/src/CSVary.cpp
@@ -0,0 +1,68 @@
+#include "CSVary.h"
+
+CSVary::CSVary() {	
+	this->_data = "";
+	this->_line = "";
+	this->_header = "";
+}
+
+String CSVary::get() {
+	return this->getheader() + "\n" + this->getrows();
+}
+
+String CSVary::getrows() {
+	this->_data = this->_data + this->_line;
+	String response = this->_data;
+	this->_line = "";
+	return response;
+}
+
+String CSVary::getheader() {
+	return this->_header;
+}
+
+CSVary& CSVary::clear() {
+	this->_data = "";
+	this->_line = "";
+	this->_header = "";
+    return *this;
+}
+
+CSVary& CSVary::setheader(String header) {
+	if(this->_header.length() == 0) {
+		this->_header = header;
+	}
+    return *this;
+}
+
+CSVary& CSVary::set(int value) {
+    return set(String(value));
+}
+
+CSVary& CSVary::set(float value) {
+    return set(String(value));
+}
+
+CSVary& CSVary::set(double value) {
+    return set(String(value));
+}
+
+CSVary& CSVary::set(String value) {
+    this->_line = this->_line + (this->_line.length() == 0 ?  "" : ",") + value;
+    return *this;
+}
+
+CSVary& CSVary::crlf() {
+	this->_data = this->_data + this->_line;
+	this->_line = "";
+    return *this;
+}
+
+// Convert String to char*
+char* CSVary::s2c(String str){
+    if(str.length()!=0){
+        char *p = const_cast<char*>(str.c_str());
+        return p;
+    }
+    return nullptr;
+}
